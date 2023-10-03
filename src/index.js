e2b20c11adb97f7b3ebe2528ab298c1684c6b1c8
KonnyGuo <guokonny@gmail.com>
2023-10-02 23:20:14 -0400

    book

--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// function Greeting() {
-//   return <h2>My First Component</h2>;
-// }
-
 // using jsx, always need to return single element (parent)
 // <></> or <React.Fragment></React.Fragment>
 
-function Greeting() {
-  // provide 3 things, element want to create, props, value inside element
-  // return React.createElement("h2", {}, "First Component");
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h2", {}, "First Component")
+const BookList = () => {
+  return (
+    <section>
+      <Book />
+    </section>
   );
-}
+};
 
-export default Greeting;
+//components
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+//implict return
+const Image = () => <h2> Image placeholder</h2>;
+const Title = () => <h2> Book Title</h2>;
+const Author = () => {
+  return <h4> Author </h4>;
+};
 
 //root for html id
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting />);
+root.render(<BookList />);
