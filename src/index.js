5732aba8191eec4da24f0f0d33fde4607b821570
KonnyGuo <guokonny@gmail.com>
2023-10-03 21:51:29 -0400

    styles

--- src/index.js
+++ src/index.js
@@ -5,36 +5,50 @@ import "./index.css";
 // using jsx, always need to return single element (parent)
 // <></> or <React.Fragment></React.Fragment>
 // no class in react, its className
-const BookList = () => {
+function BookList() {
   return (
-    <section className="bookList">
+    <section className="booklist">
       <Book />
       <Book />
       <Book />
       <Book />
     </section>
   );
-};
+}
 
 //components
 const Book = () => {
+  const title = "How to Catch a Dinosaur";
   return (
-    <article className="bookList">
-      <Image />
-      <Title />
+    <article className="book">
+      <img src="amazonBookimg1.jpg" alt="How to Catch a Dinosaur" />
+      <h2> {title} </h2>
       <Author />
     </article>
   );
 };
 
-//implict return
-const Image = () => (
-  <img src="amazonBookimg1.jpg" alt="How to Catch a Dinosaur" />
-);
-const Title = () => <h2> Book Title</h2>;
 const Author = () => {
-  return <h4> Adam Wallace (Author), Andy Elkerton (Illustrator) </h4>;
+  const author = "Adam Wallace (Author), Andy Elkerton (Illustrator)";
+  // creating obj instead of passing style directly
+  const inlneHeadingStyles = {
+    color: "#617d98",
+    fontSize: "0.75rem",
+    marginTop: "0.5rem",
+  };
+  return <h4 style={inlneHeadingStyles}>{author}</h4>;
 };
+
+// need {{}} double curlies for objects
+// external lib use inline css, need to modify inline style
+// const Author = () => {
+//   return (
+//     <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.5rem" }}>
+//       {" "}
+//       Adam Wallace (Author), Andy Elkerton (Illustrator){" "}
+//     </h4>
+//   );
+// };
 
 //root for html id
 const root = ReactDOM.createRoot(document.getElementById("root"));
