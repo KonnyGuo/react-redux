631d859a9f65969297a45c835dca81920cce7d76
KonnyGuo <guokonny@gmail.com>
2023-10-08 20:46:47 -0400

    fixing invoking

--- src/index.js
+++ src/index.js
@@ -21,20 +21,20 @@ const books = [
 // no class in react, its className
 // if we want to render something between <Book> </Book> need to use special prop children- name must be like this exactly, is provided by react
 function BookList() {
-  const getBook1 = (id) => {
+  const getBook = (id) => {
     const bookByID = books.find((element) => {
-      return element.id == id
-    })
-    console.log(bookByID)
-    return bookByID
-  }
+      return element.id == id;
+    });
+    console.log(bookByID);
+    return bookByID;
+  };
   return (
     <section className="booklist">
       {books.map((book) => {
         // const { img, title, author, id } = book;
         // always remember key value to be put somewhere
         // return <Book img={img} title={title} author={author} key={id} />;
-        return <Book {...book} key={book.id}/>;
+        return <Book {...book} key={book.id} getBookProp={getBook} />;
       })}
     </section>
   );
@@ -49,12 +49,12 @@ function BookList() {
 // props are objects, it is only display when value is provided
 // {job, title, number} destructure in parameter
 // {job, title, number} = props destructure as variable
-const Book = ({ img, title, author }) => {
+const Book = ({ img, title, author, getBookProp }) => {
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
-      <button onClick={}> Click this</button>
+      <button onClick={getBookProp}> Click this</button>
       <h4>{author} </h4>
     </article>
   );
