180a7e76db6d319497b37c29a862ede9ea25b96b
KonnyGuo <guokonny@gmail.com>
2023-10-10 22:29:15 -0400

    import change

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { books } from "./books.js";
-import { Book } from "./Book.js";
-
+import Book from "./Book.js";
 // using jsx, always need to return single element (parent)
 // <></> or <React.Fragment></React.Fragment>
 // no class in react, its className
